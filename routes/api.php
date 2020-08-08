4958955a962a9a58b2403f750a75b43d43d45ebe
Ash Duckett <ash.duckett@outlook.com>
2020-08-08 18:01:20 +0100

    Add User route

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Routes for managing users
+Route::resource('users', 'User\UserController')->except(['create', 'edit']);
